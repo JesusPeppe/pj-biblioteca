77046835592c31d6f313a725ff63d5f5d7eb09e6
Salvatore <toresalva99@libero.it>
2025-02-14 14:56:49 +0100

    First Commit

--- proj_library/library_app/admin.py
+++ proj_library/library_app/admin.py
@@ -34,3 +34,4 @@ class CustomAdmin(admin.AdminSite):
         
 admin.site = CustomAdmin()   
 '''
+
--- proj_library/library_app/models.py
+++ proj_library/library_app/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
